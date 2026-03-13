e49eaa46de3886bf762ffa1d3e08a3b2de11311a
JEAN LUCCA GARDIANO DOS SANTOS <jean.lgsantos@senacsp.edu.br>
2026-03-13 17:01:09 -0300

    dudu

--- modelodemenu/Form1.Designer.cs
+++ modelodemenu/Form1.Designer.cs
@@ -31,31 +31,30 @@
             this.components = new System.ComponentModel.Container();
             this.Sidebar = new System.Windows.Forms.FlowLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuButton = new System.Windows.Forms.PictureBox();
+            this.HomeContainer = new System.Windows.Forms.Panel();
+            this.buttonHome = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.SidebarTime = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.Sidebar.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.menuButton)).BeginInit();
+            this.HomeContainer.SuspendLayout();
             this.SuspendLayout();
             // 
             // Sidebar
             // 
             this.Sidebar.BackColor = System.Drawing.Color.Orange;
             this.Sidebar.Controls.Add(this.panel1);
-            this.Sidebar.Controls.Add(this.panel2);
-            this.Sidebar.Controls.Add(this.button2);
-            this.Sidebar.Controls.Add(this.button3);
-            this.Sidebar.Controls.Add(this.button4);
             this.Sidebar.Dock = System.Windows.Forms.DockStyle.Left;
             this.Sidebar.Location = new System.Drawing.Point(0, 0);
             this.Sidebar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Sidebar.MaximumSize = new System.Drawing.Size(284, 727);
+            this.Sidebar.MinimumSize = new System.Drawing.Size(74, 727);
             this.Sidebar.Name = "Sidebar";
             this.Sidebar.Size = new System.Drawing.Size(284, 727);
             this.Sidebar.TabIndex = 0;
@@ -63,61 +62,72 @@
             // panel1
             // 
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.menuButton);
             this.panel1.Location = new System.Drawing.Point(4, 5);
             this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(279, 162);
             this.panel1.TabIndex = 0;
             // 
-            // pictureBox1
+            // label1
             // 
-            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Image = global::modelodemenu.Properties.Resources.cardapio;
-            this.pictureBox1.Location = new System.Drawing.Point(30, 63);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(24, 24);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "  Menu";
             // 
-            // panel2
+            // menuButton
             // 
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Location = new System.Drawing.Point(4, 177);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(285, 54);
-            this.panel2.TabIndex = 2;
+            this.menuButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuButton.Image = global::modelodemenu.Properties.Resources.cardapio;
+            this.menuButton.Location = new System.Drawing.Point(30, 63);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(24, 24);
+            this.menuButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.menuButton.TabIndex = 0;
+            this.menuButton.TabStop = false;
+            this.menuButton.Click += new System.EventHandler(this.menuButton_Click);
             // 
-            // button1
+            // HomeContainer
             // 
-            this.button1.BackColor = System.Drawing.Color.Orange;
-            this.button1.Image = global::modelodemenu.Properties.Resources.botao_de_inicio;
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(-4, 5);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
-            this.button1.Size = new System.Drawing.Size(289, 56);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "       Home";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.HomeContainer.BackColor = System.Drawing.Color.Orange;
+            this.HomeContainer.Controls.Add(this.button1);
+            this.HomeContainer.Controls.Add(this.buttonHome);
+            this.HomeContainer.Location = new System.Drawing.Point(343, 349);
+            this.HomeContainer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.HomeContainer.Name = "HomeContainer";
+            this.HomeContainer.Size = new System.Drawing.Size(285, 142);
+            this.HomeContainer.TabIndex = 2;
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.BackColor = System.Drawing.Color.Orange;
+            this.buttonHome.Image = global::modelodemenu.Properties.Resources.botao_de_inicio;
+            this.buttonHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonHome.Location = new System.Drawing.Point(0, 0);
+            this.buttonHome.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.buttonHome.Size = new System.Drawing.Size(285, 56);
+            this.buttonHome.TabIndex = 1;
+            this.buttonHome.Text = "         Home";
+            this.buttonHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonHome.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Orange;
             this.button2.Image = global::modelodemenu.Properties.Resources.engrenagem;
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(4, 241);
+            this.button2.Location = new System.Drawing.Point(853, 504);
             this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
             this.button2.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
             this.button2.Size = new System.Drawing.Size(289, 56);
             this.button2.TabIndex = 3;
-            this.button2.Text = "       Settinggs";
+            this.button2.Text = "        Settinggs";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button2.UseVisualStyleBackColor = false;
             // 
@@ -126,13 +136,13 @@
             this.button3.BackColor = System.Drawing.Color.Orange;
             this.button3.Image = global::modelodemenu.Properties.Resources.solicitacao;
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(4, 307);
+            this.button3.Location = new System.Drawing.Point(853, 570);
             this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
             this.button3.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
             this.button3.Size = new System.Drawing.Size(289, 56);
             this.button3.TabIndex = 4;
-            this.button3.Text = "       Help";
+            this.button3.Text = "        Help";
             this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button3.UseVisualStyleBackColor = false;
             // 
@@ -141,35 +151,46 @@
             this.button4.BackColor = System.Drawing.Color.Orange;
             this.button4.Image = global::modelodemenu.Properties.Resources.em_formacao;
             this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button4.Location = new System.Drawing.Point(4, 373);
+            this.button4.Location = new System.Drawing.Point(853, 636);
             this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button4.Name = "button4";
             this.button4.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
             this.button4.Size = new System.Drawing.Size(289, 56);
             this.button4.TabIndex = 5;
-            this.button4.Text = "       Aboult";
+            this.button4.Text = "        Aboult";
             this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button4.UseVisualStyleBackColor = false;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 66);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 21);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Menu";
-            // 
             // SidebarTime
             // 
+            this.SidebarTime.Interval = 10;
             this.SidebarTime.Tick += new System.EventHandler(this.SidebarTime_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orange;
+            this.button1.Image = global::modelodemenu.Properties.Resources.cardapio;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 51);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(285, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "        Sub Menu";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 727);
+            this.Controls.Add(this.HomeContainer);
             this.Controls.Add(this.Sidebar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
@@ -178,8 +199,8 @@
             this.Sidebar.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.menuButton)).EndInit();
+            this.HomeContainer.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -188,14 +209,15 @@
 
         private System.Windows.Forms.FlowLayoutPanel Sidebar;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel HomeContainer;
+        private System.Windows.Forms.Button buttonHome;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox menuButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer SidebarTime;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- modelodemenu/Form1.cs
+++ modelodemenu/Form1.cs
@@ -20,17 +20,36 @@ namespace modelodemenu
 
         private void SidebarTime_Tick(object sender, EventArgs e)
         {
+            //set the maximun and minimum size of the sidebar
+
+
+
             if (sidebarExpanded)
                 
             { 
                 //if sidebar is expand, mininiza
             Sidebar.Width -= 10;
-                if(Sidebar.Width -- sidebar.ManimumSize.Width)
+                if(Sidebar.Width <= Sidebar.MinimumSize.Width)
                 {
                     sidebarExpanded = false;
                     SidebarTime.Stop();
                 }
             }
+            else
+            {
+                Sidebar.Width += 10;
+                if(Sidebar.Width >= Sidebar.MaximumSize.Width)
+                {
+                    sidebarExpanded = true;
+                    SidebarTime.Stop();
+                }
+            }
+        }
+
+        private void menuButton_Click(object sender, EventArgs e)
+        {
+            //set timer interval to lowest to make it smooth
+            SidebarTime.Start();
         }
     }
 }
