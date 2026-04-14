148250890d636eda38304acf1c92998e0fd1c49d
JEAN LUCCA GARDIANO DOS SANTOS <jean.lgsantos@senacsp.edu.br>
2026-04-14 16:11:02 -0300

    bancodedadosteste

---
+++ Banco de dados teste/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Banco_de_dados_teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtCodigoUsuario = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblCodigousuario = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.lblCargo = new System.Windows.Forms.Label();
+            this.txtCargo = new System.Windows.Forms.TextBox();
+            this.lblNomecompleto = new System.Windows.Forms.Label();
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.lblCadastra = new System.Windows.Forms.Label();
+            this.lblConfimarsenha = new System.Windows.Forms.Label();
+            this.txtConfirmarSenha = new System.Windows.Forms.TextBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.lblTipodeacesso = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.lblTipodeacesso);
+            this.panel1.Controls.Add(this.btnConfirmar);
+            this.panel1.Controls.Add(this.txtConfirmarSenha);
+            this.panel1.Controls.Add(this.lblConfimarsenha);
+            this.panel1.Controls.Add(this.txtSenha);
+            this.panel1.Controls.Add(this.txtCodigoUsuario);
+            this.panel1.Controls.Add(this.lblSenha);
+            this.panel1.Controls.Add(this.lblCodigousuario);
+            this.panel1.Controls.Add(this.txtTelefone);
+            this.panel1.Controls.Add(this.lblTelefone);
+            this.panel1.Controls.Add(this.lblCargo);
+            this.panel1.Controls.Add(this.txtCargo);
+            this.panel1.Controls.Add(this.lblNomecompleto);
+            this.panel1.Controls.Add(this.txtNomeCompleto);
+            this.panel1.Controls.Add(this.lblCadastra);
+            this.panel1.Location = new System.Drawing.Point(196, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(418, 571);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(101)))), ((int)(((byte)(134)))));
+            this.txtSenha.Location = new System.Drawing.Point(15, 357);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(390, 38);
+            this.txtSenha.TabIndex = 18;
+            // 
+            // txtCodigoUsuario
+            // 
+            this.txtCodigoUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+            this.txtCodigoUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigoUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(101)))), ((int)(((byte)(134)))));
+            this.txtCodigoUsuario.Location = new System.Drawing.Point(15, 292);
+            this.txtCodigoUsuario.Name = "txtCodigoUsuario";
+            this.txtCodigoUsuario.Size = new System.Drawing.Size(390, 38);
+            this.txtCodigoUsuario.TabIndex = 16;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.ForeColor = System.Drawing.Color.White;
+            this.lblSenha.Location = new System.Drawing.Point(20, 333);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(53, 21);
+            this.lblSenha.TabIndex = 15;
+            this.lblSenha.Text = "Senha";
+            // 
+            // lblCodigousuario
+            // 
+            this.lblCodigousuario.AutoSize = true;
+            this.lblCodigousuario.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigousuario.ForeColor = System.Drawing.Color.White;
+            this.lblCodigousuario.Location = new System.Drawing.Point(18, 268);
+            this.lblCodigousuario.Name = "lblCodigousuario";
+            this.lblCodigousuario.Size = new System.Drawing.Size(118, 21);
+            this.lblCodigousuario.TabIndex = 14;
+            this.lblCodigousuario.Text = "Código Usuário";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+            this.txtTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefone.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(101)))), ((int)(((byte)(134)))));
+            this.txtTelefone.Location = new System.Drawing.Point(15, 227);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(390, 38);
+            this.txtTelefone.TabIndex = 13;
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefone.ForeColor = System.Drawing.Color.White;
+            this.lblTelefone.Location = new System.Drawing.Point(18, 203);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(69, 21);
+            this.lblTelefone.TabIndex = 12;
+            this.lblTelefone.Text = "Telefone";
+            // 
+            // lblCargo
+            // 
+            this.lblCargo.AutoSize = true;
+            this.lblCargo.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCargo.ForeColor = System.Drawing.Color.White;
+            this.lblCargo.Location = new System.Drawing.Point(20, 138);
+            this.lblCargo.Name = "lblCargo";
+            this.lblCargo.Size = new System.Drawing.Size(52, 21);
+            this.lblCargo.TabIndex = 7;
+            this.lblCargo.Text = "Cargo";
+            // 
+            // txtCargo
+            // 
+            this.txtCargo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+            this.txtCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCargo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(101)))), ((int)(((byte)(134)))));
+            this.txtCargo.Location = new System.Drawing.Point(15, 162);
+            this.txtCargo.Name = "txtCargo";
+            this.txtCargo.Size = new System.Drawing.Size(150, 38);
+            this.txtCargo.TabIndex = 6;
+            // 
+            // lblNomecompleto
+            // 
+            this.lblNomecompleto.AutoSize = true;
+            this.lblNomecompleto.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomecompleto.ForeColor = System.Drawing.Color.White;
+            this.lblNomecompleto.Location = new System.Drawing.Point(19, 73);
+            this.lblNomecompleto.Name = "lblNomecompleto";
+            this.lblNomecompleto.Size = new System.Drawing.Size(125, 21);
+            this.lblNomecompleto.TabIndex = 5;
+            this.lblNomecompleto.Text = "Nome Completo";
+            // 
+            // txtNomeCompleto
+            // 
+            this.txtNomeCompleto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+            this.txtNomeCompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeCompleto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(101)))), ((int)(((byte)(134)))));
+            this.txtNomeCompleto.Location = new System.Drawing.Point(15, 97);
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(390, 38);
+            this.txtNomeCompleto.TabIndex = 4;
+            // 
+            // lblCadastra
+            // 
+            this.lblCadastra.AutoSize = true;
+            this.lblCadastra.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastra.Location = new System.Drawing.Point(116, 27);
+            this.lblCadastra.Name = "lblCadastra";
+            this.lblCadastra.Size = new System.Drawing.Size(180, 25);
+            this.lblCadastra.TabIndex = 0;
+            this.lblCadastra.Text = "Cadrastra Usuario";
+            // 
+            // lblConfimarsenha
+            // 
+            this.lblConfimarsenha.AutoSize = true;
+            this.lblConfimarsenha.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfimarsenha.ForeColor = System.Drawing.Color.White;
+            this.lblConfimarsenha.Location = new System.Drawing.Point(19, 398);
+            this.lblConfimarsenha.Name = "lblConfimarsenha";
+            this.lblConfimarsenha.Size = new System.Drawing.Size(128, 21);
+            this.lblConfimarsenha.TabIndex = 20;
+            this.lblConfimarsenha.Text = "Confirmar Senha";
+            // 
+            // txtConfirmarSenha
+            // 
+            this.txtConfirmarSenha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+            this.txtConfirmarSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmarSenha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(101)))), ((int)(((byte)(134)))));
+            this.txtConfirmarSenha.Location = new System.Drawing.Point(15, 422);
+            this.txtConfirmarSenha.Name = "txtConfirmarSenha";
+            this.txtConfirmarSenha.Size = new System.Drawing.Size(390, 38);
+            this.txtConfirmarSenha.TabIndex = 21;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(217)))), ((int)(((byte)(95)))));
+            this.btnConfirmar.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmar.Location = new System.Drawing.Point(82, 488);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(252, 32);
+            this.btnConfirmar.TabIndex = 22;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = false;
+            // 
+            // lblTipodeacesso
+            // 
+            this.lblTipodeacesso.AutoSize = true;
+            this.lblTipodeacesso.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipodeacesso.ForeColor = System.Drawing.Color.White;
+            this.lblTipodeacesso.Location = new System.Drawing.Point(292, 138);
+            this.lblTipodeacesso.Name = "lblTipodeacesso";
+            this.lblTipodeacesso.Size = new System.Drawing.Size(113, 21);
+            this.lblTipodeacesso.TabIndex = 23;
+            this.lblTipodeacesso.Text = "Tipo de Acesso";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(207)))), ((int)(((byte)(207)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(255, 162);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(150, 21);
+            this.comboBox1.TabIndex = 24;
+            this.comboBox1.Text = "Selecione";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 690);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Label lblCadastra;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblCodigousuario;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Label lblCargo;
+        private System.Windows.Forms.TextBox txtCargo;
+        private System.Windows.Forms.Label lblNomecompleto;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtCodigoUsuario;
+        private System.Windows.Forms.Label lblConfimarsenha;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblTipodeacesso;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.TextBox txtConfirmarSenha;
+    }
+}
+
---
+++ teste cadastro/Form1.cs
@@ -0,0 +1,200 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace crud
+
+{
+
+    public partial class frmCadastrodeFuncionarios : Form
+
+    {
+
+        //Conexão com o banco de dados MySQL
+
+        MySqlConnection Conexao;
+
+        string data_source = "datasource=localhost; username=root; database=db_funcionarios";
+
+        public frmCadastrodeFuncionarios()
+
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+
+        {
+
+            try
+
+            {
+
+                //Validando campos obrigatórios
+
+                if (string.IsNullOrEmpty(txtnome_funcionario.Text.Trim()) ||
+
+                   string.IsNullOrEmpty(txtcargo.Text.Trim()) ||
+
+                   string.IsNullOrEmpty(txttelefone.Text.Trim()) ||
+
+                   string.IsNullOrEmpty(txttipo_acesso.Text.Trim()) ||
+
+                   string.IsNullOrEmpty(txtsenha.Text.Trim()))
+                {
+
+                    MessageBox.Show("Todos os campos devem ser preenchidos.",
+
+                                      "Validação",
+
+                                      MessageBoxButtons.OK,
+
+                                      MessageBoxIcon.Warning);
+
+                    return; //Impede o prosseguimento se algum campo estiver vazio
+
+                }
+
+                //Validação do CPF
+
+                string senha = txtsenha.Text.Trim();
+
+                if (!isValidsenhaLength(senha))
+
+                {
+
+                    MessageBox.Show("senha inválida.",
+
+                        "Validação de senha",
+
+                        MessageBoxButtons.OK,
+
+                        MessageBoxIcon.Warning);
+
+                    return; //Impede o prosseguimento se a senha for inválida
+
+                }
+
+                //Cria a conexão com o banco de dados
+
+                Conexao = new MySqlConnection(data_source);
+
+                Conexao.Open();
+
+                //Comando SQL para inserir um novo cliente no banco de dados
+
+                MySqlCommand cmd = new MySqlCommand
+
+                {
+
+                    Connection = Conexao
+
+                };
+
+                cmd.Prepare();
+
+                cmd.CommandText = "INSERT INTO funcionarios(id_funcionario, nome_funcionario, cargo, telefone, tipo_acesso, senha) " +
+
+                "VALUES(@nomecompleto, @nomesocial, @email, @cpf)";
+
+                //Adiciona os parâmetros com os dados do formulário
+
+                cmd.Parameters.AddWithValue("@nome_funcionario", txtnome_funcionario.Text.Trim());
+
+                cmd.Parameters.AddWithValue("@cargo", txtcargo.Text.Trim());
+
+                cmd.Parameters.AddWithValue("@telefone", txttelefone.Text.Trim());
+
+                cmd.Parameters.AddWithValue("@tipo_acesso", txttipo_acesso.Text.Trim());
+
+                cmd.Parameters.AddWithValue("@senha", txtsenha.Text.Trim());
+
+                //Executa o comando de inserção no banco
+
+                cmd.ExecuteNonQuery();
+
+                //Mensagem de sucesso
+
+                MessageBox.Show("Contato inserido com sucesso:",
+
+                                "Sucesso",
+
+                                MessageBoxButtons.OK,
+
+                                MessageBoxIcon.Information);
+
+            }
+
+            catch (MySqlException ex)
+
+            {
+
+                //Trata erros relacionados ao MySQL
+
+                MessageBox.Show("Erro " + ex.Number + " ocorreu: " + ex.Message,
+
+                                "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                //Trata outros tipos de erro
+
+                MessageBox.Show("Ocorreu: " + ex.Message,
+
+                                "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+
+
+            finally
+
+            {
+
+                //Garante que a conexão com o banco será fechada, mesmo se ocorrer erro
+
+                if (Conexao != null && Conexao.State == ConnectionState.Open)
+
+                {
+
+                    Conexao.Close();
+
+                }
+
+            }
+
+        }
+
+        private bool isValidCPFLength(string cpf)
+
+        {
+
+            // Remove todos os caracteres não númericos
+
+            cpf = new string(cpf.Where(char.IsDigit).ToArray());
+
+            // Verifica se o CPF tem exatamente 11 dígitos;
+
+            return cpf.Length == 11;
+
+        }
+
+    }
+
+}
+
+
